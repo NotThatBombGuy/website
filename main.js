e820030f54599600cae4b0115a68755f239d36b9
NotThatBombGuy <notthatbombguyyt@gmail.com>
2026-06-28 21:53:46 -0600

    test

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
-const siteName = "the site of bomb";
-let visitorCount = 0;
+// changes greeting based on the time of day
+
+const heading = document.querySelector(".title-text");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+  heading.textContent = "Good morning - assume I'm not working on this.";
+} else if (hour < 18) {
+  heading.textContent = "Good afternoon - I'm probably being lazy and not working on this.";
+} else {
+  heading.textContent = "Good evening - I might be working on ts but I'm probably not.";
+}
+
+// changes logo text to a random message when clicked
+
+const messages = [
+    "you found the secret",
+    "bomb dot com goes hard",
+    "why are you clicking me"
+  ];
+  
+  const logo = document.querySelector(".logo a");
+  const chance = 0.1;
+  
+  logo.addEventListener("click", function (event) {
+    if (Math.random() < chance) {
+      event.preventDefault();
+  
+      const random = messages[Math.floor(Math.random() * messages.length)];
+      logo.textContent = random;
+    }
+  });
+
+  function fitLogoText(linkElement, maxSize = 35, minSize = 14) {
+    linkElement.style.fontSize = maxSize + "px";
+  
+    const container = linkElement.parentElement;
+  
+    while (
+      linkElement.scrollWidth > container.clientWidth &&
+      parseFloat(linkElement.style.fontSize) > minSize
+    ) {
+      linkElement.style.fontSize =
+        parseFloat(linkElement.style.fontSize) - 1 + "px";
+    }
+  }
